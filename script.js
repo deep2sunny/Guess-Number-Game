c6abc7a6ecf47f46a6eff2829b8e2c0cc19e5de8
Akashdeep Singh <deep2sunny@gmail.com>
2020-12-30 19:25:48 -0500

    Implement Again Button functionality to reset the game

--- script.js
+++ script.js
@@ -18,22 +18,31 @@ console.log(document.querySelector('.guess').value);
 */
 
 //number between 1 and 20
-const secretNumber = Math.trunc(Math.random()*20)+1;
+let secretNumber = Math.trunc(Math.random()*20)+1;
 
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
 
-//Event
+
+//Check number button functionality
 document.querySelector('.check').addEventListener('click', function() {
   const guess = Number(document.querySelector('.guess').value);
 //  document.querySelector('.message').textContent = 'Correct Number!';
   console.log(guess, typeof guess);
 
+  //When there is no input
   if(!guess) {
     document.querySelector('.message').textContent = 'No Number!';
+
+    //When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    //When guess is too high
   } else if (guess > secretNumber) {
     if(score > 1){
       document.querySelector('.message').textContent = 'Too high!';
@@ -43,6 +52,8 @@ document.querySelector('.check').addEventListener('click', function() {
       document.querySelector('.message').textContent = 'You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    //When guess is too low
   } else if (guess < secretNumber) {
     if(score > 1){
       document.querySelector('.message').textContent = 'Too low!';
@@ -51,6 +62,18 @@ document.querySelector('.check').addEventListener('click', function() {
     } else {
       document.querySelector('.message').textContent = 'You lost the game!';
       document.querySelector('.score').textContent = 0;
-    } 
+    }
   }
+});
+
+// Again button functionality, Reset the game
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random()*20)+1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
